bfe36754b6478089a08e6cc4ae012f4fb68ea34d
iBubnov <illiabubnov@gmail.com>
2019-09-21 19:34:37 +0300

    new

---
+++ js/common.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		items:1,
+		margin:10,
+		nav:true,
+	})
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+});
