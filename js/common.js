5860185cfec2bcb189c678cb8bdbef746653dfa3
iBubnov <illiabubnov@gmail.com>
2019-09-22 01:07:51 +0300

    new

--- js/common.js
+++ js/common.js
@@ -5,18 +5,33 @@ $(document).ready(function(){
 		items:1,
 		margin:10,
 		nav:true,
-	})
+    })
+
+    //iziModal
+    $(".modal").iziModal({
+        radius: 2,
+        headerColor: '#666666',
+        padding: 15,
+        closeOnEscape: true,
+        closeButton: true,
+        zindex: 9999,
+        transitionIn: 'comingIn',
+        transitionOut: 'comingOut',
+        transitionInOverlay: 'fadeIn',
+        transitionOutOverlay: 'fadeOut',
+    });
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$(".sendform").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+            alert("Thank you!");
+            $('.modal').iziModal('close');
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
@@ -35,11 +50,10 @@ $(document).ready(function(){
 
 	};
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
+    
+    $(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
 	
 });
 
-$(window).load(function() {
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-});
